5a993cfd793dfd47ac056378d415da66df39c796
Pablo Peña <ppena@fintonic.com>
2024-01-09 00:53:47 -0600

    initial commit

---
+++ src/main/java/mx/prueba/loansmanager/LoansManagerApplication.java
@@ -0,0 +1,13 @@
+package mx.prueba.loansmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/prueba/loansmanager/LoansManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.prueba.loansmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
